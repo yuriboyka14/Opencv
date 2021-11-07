5788c44afe056df7fc9b1f862362ca378fb6604e
yuriboyka14 <podkowinka14@gmail.com>
2021-11-07 14:28:09 +0100

    test commit

--- field.py
+++ field.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+
+# dodany komatarz dla sprawdzenia
 
 class FieldChecker:
     def __init__(self, image):
@@ -37,6 +40,22 @@ class FieldChecker:
 
         img = cv2.imread(self.image)
         gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+        thresh = cv2.threshold(gray, 165, 255, cv2.THRESH_BINARY)[1]
+
+        #image manipulating
+        # gray2 = gray.copy()
+        # rows = gray2.shape[0]
+        # cols = gray2.shape[1]
+        # for row in range(0, rows):
+        #     for col in range(0, cols):
+        #         if gray2[row, col] < 100:
+        #             gray2[row, col] = 0
+        #
+        # plt.imshow(gray2)
+        # plt.show()
+        #image manipulating end
+
         # blur = cv2.GaussianBlur(gray, (3, 3), 0)
         edges = cv2.Canny(gray, 80, 150, apertureSize=3)
 
@@ -45,7 +64,9 @@ class FieldChecker:
         self.set_height()
         self.set_threshold()
 
+        # lines = cv2.HoughLines(edges, 1, np.pi / 180, threshold=100)
         lines = cv2.HoughLines(edges, 1, np.pi / 180, threshold=int(self.threshold))
+        # lines = cv2.HoughLines(edges, 1, math.pi / 180.0, 165, np.array([]), 0, 0)
         lines_size = len(lines)
 
         for i in range(0, lines_size):
@@ -66,11 +87,10 @@ class FieldChecker:
 
         plt.imshow(img)
         plt.show()
+        plt.imshow(gray)
+        plt.show()
         plt.imshow(edges)
         plt.show()
-        cv2.imshow("edges image", edges)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
 
         vertical = []
         horizontal = []
@@ -121,6 +141,55 @@ class FieldChecker:
             return True
 
 
+    def auxilary_function(self):
+        # read image
+        img = cv2.imread(self.image)
+
+        # convert to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # threshold
+        thresh = cv2.threshold(gray, 165, 255, cv2.THRESH_BINARY)[1]
+
+        # apply close to connect the white areas
+        kernel = np.ones((15, 1), np.uint8)
+        morph = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+        kernel = np.ones((17, 3), np.uint8)
+        morph = cv2.morphologyEx(morph, cv2.MORPH_CLOSE, kernel)
+
+        # apply canny edge detection
+        edges = cv2.Canny(img, 175, 200)
+
+        # get hough lines
+        result = img.copy()
+        lines = cv2.HoughLines(edges, 1, math.pi / 180.0, 165, np.array([]), 0, 0)
+        a, b, c = lines.shape
+        for i in range(a):
+            rho = lines[i][0][0]
+            theta = lines[i][0][1]
+            a = math.cos(theta)
+            b = math.sin(theta)
+            x0, y0 = a * rho, b * rho
+            pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
+            pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
+            cv2.line(result, pt1, pt2, (0, 0, 255), 2, cv2.LINE_AA)
+
+        # save resulting images
+        cv2.imwrite('fabric_equalized_thresh.jpg', thresh)
+        cv2.imwrite('fabric_equalized_morph.jpg', morph)
+        cv2.imwrite('fabric_equalized_edges.jpg', edges)
+        cv2.imwrite('fabric_equalized_lines.jpg', result)
+
+        # show thresh and result
+        cv2.imshow("thresh", thresh)
+        cv2.imshow("morph", morph)
+        cv2.imshow("edges", edges)
+        cv2.imshow("result", result)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
 if __name__ == "__main__":
-    checker = FieldChecker('img/sc2.png')
+    checker = FieldChecker('img/ttt_3.png')
     checker.field_recognition()
+    # checker.auxilary_function()

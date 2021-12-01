5cb040cf7e7f7c068d2cb243d9d65e8e15b2dac6
yuriboyka14 <podkowinka14@gmail.com>
2021-12-01 22:16:35 +0100

    board_detection + game logic beginnings

--- dominik_board_detection.py
+++ dominik_board_detection.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+from game import Game, findBestMove
+
 
 def preprocess(img):
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
@@ -8,16 +10,17 @@ def preprocess(img):
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
     edges = cv2.Canny(gray, 40, 170)
-    dillated_edges = cv2.dilate(edges.copy(), None, iterations = 5)
-    dillated_edges = cv2.morphologyEx(dillated_edges, cv2.MORPH_CLOSE, np.ones((5,5),np.uint8))
+    dillated_edges = cv2.dilate(edges.copy(), None, iterations=5)
+    dillated_edges = cv2.morphologyEx(dillated_edges, cv2.MORPH_CLOSE, np.ones((5, 5), np.uint8))
 
-    return dillated_edges 
+    return dillated_edges
 
 
 def find_circle(processed_img, output_img, last_circles):
     rows = processed_img.shape[0]
-    circles = cv2.HoughCircles(processed_img, cv2.HOUGH_GRADIENT, 1, rows/8, param1=100, param2=30,minRadius=10, maxRadius=100)
-    
+    circles = cv2.HoughCircles(processed_img, cv2.HOUGH_GRADIENT, 1, rows / 8, param1=100, param2=30, minRadius=10,
+                               maxRadius=100)
+
     if circles is not None:
         circles = np.uint16(np.around(circles))
 
@@ -33,11 +36,10 @@ def print_cricles(img, circles):
 
 
 def circle_position(circles, cnt, img):
-    grid_return = np.zeros((3,3), dtype=bool)
+    grid_return = np.zeros((3, 3), dtype=str)  # change to str from bool
     height = img.shape[1]
-    xd = np.zeros((3,3), dtype=bool)
+    xd = np.zeros((3, 3), dtype=bool)
 
-    
     if cnt is not None:
         if circles is not None:
             leftup_x = cnt[3][0][0]
@@ -55,80 +57,132 @@ def circle_position(circles, cnt, img):
             for cr in circles[0, :]:
                 (x, y) = (cr[0], cr[1])
 
-                grid_x = np.zeros((3,3), dtype=bool)
-                grid_y = np.zeros((3,3), dtype=bool)
+                grid_x = np.zeros((3, 3), dtype=str)   # change to str from bool
+                grid_y = np.zeros((3, 3), dtype=str)   # change to str from bool
 
-                #grid_x
-                if x <= leftup_x:
-                    grid_x[:][0] = True
-                
-                elif x >= leftup_x and x <= rightdown_x:
-                    grid_x[:][1] = True
-                
+                # grid_x
+                if x < leftup_x:
+                    grid_x[:][0] = "o"
+
+                elif leftup_x <= x < rightdown_x:
+                    grid_x[:][1] = "o"
+
                 else:
-                    grid_x[:][2] = True
+                    grid_x[:][2] = "o"
 
-                #grid_y
+                # grid_y
                 if y > leftdown_y:
-                    grid_y[0][:] = True
-                
-                elif y <= leftdown_y and y >= rightup_y:
-                    grid_y[1][:] = True
-                
+                    grid_y[0][:] = "o"
+
+                elif leftdown_y >= y > rightup_y:
+                    grid_y[1][:] = "o"
+
                 else:
-                    grid_y[2][:] = True
+                    grid_y[2][:] = "o"
 
                 grid_x = np.rot90(grid_x)
 
-                #grid
+                # grid
                 for t in range(3):
                     for r in range(3):
-                        grid_return[t][r] = grid_return[t][r] or (grid_x[t][r] and grid_y[t][r]) 
-    
+                        grid_return[t][r] = grid_return[t][r] or (grid_x[t][r] and grid_y[t][r])
+
         else:
             print("No circles!")
     else:
         print("No contour!")
-    
+
     return grid_return
 
 
-vid = cv2.VideoCapture(0)
-last_contour = None
-last_circles = None
+if __name__ == "__main__":
 
-while True:
-    ret, img = vid.read()
-    processed_img = preprocess(img)
+    vid = cv2.VideoCapture(0)
+    last_contour = None
+    last_circles = None
 
-    contours, _ = cv2.findContours(processed_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    best_move_done = []     # array which will contain positions of computer player
+                            # it will be used to display the x
 
-    for c in contours:
-        accuracy = 0.01 * cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, accuracy, True)
-        
-        if len(approx) == 4 and cv2.contourArea(approx) > 15000:
-            last_contour = approx
-            cv2.drawContours(img, [approx], -1, (120,255,155), 10)
-        
-        elif last_contour is not None:
-            cv2.drawContours(img, [last_contour], -1, (150,255,155), 3)
+    x_pos = [[(100, 150), (100, 400), (100, 650)],        # possible x positions (each square)
+               [(600, 150), (600, 400), (600, 650)],
+               [(1100, 150), (1100, 400), (1100, 650)]]
 
-    print_cricles(img, last_circles)
+    while True:
+        ret, img = vid.read()
+        processed_img = preprocess(img)
 
-    k = cv2.waitKey(2)
+        contours, _ = cv2.findContours(processed_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 
-    if k & 0xFF == ord('q'):
-        if grid is not None:
-            print(grid)
-        break
+        # --- experimenting with text --- #
 
-    if k & 0xFF == ord('\r'):
-        last_circles = find_circle(processed_img, img, last_circles)
-        grid = circle_position(last_circles, last_contour, img)
-    
-    cv2.imshow('image', img)
-    cv2.imshow('edges', processed_img)
-        
-vid.release()
-cv2.destroyAllWindows()
+        # font = cv2.FONT_HERSHEY_SIMPLEX
+        # cv2.putText(img, 'X', (100, 650), font, 4, (200, 180, 100), 6, cv2.LINE_AA)  # [2][0]
+        # cv2.putText(img, 'X', (100, 400), font, 4, (200, 180, 100), 6, cv2.LINE_AA)  # [1][0]
+        # cv2.putText(img, 'X', (100, 150), font, 4, (200, 180, 100), 6, cv2.LINE_AA)  # [0][0]
+        # cv2.putText(img, 'X', (600, 650), font, 4, (200, 180, 100), 6, cv2.LINE_AA)  # [2][1]
+        # cv2.putText(img, 'X', (600, 400), font, 4, (200, 180, 100), 6, cv2.LINE_AA)  # [1][1]
+        # cv2.putText(img, 'X', (600, 150), font, 4, (200, 180, 100), 6, cv2.LINE_AA)  # [0][1]
+        # cv2.putText(img, 'X', (1100, 650), font, 4, (200, 180, 100), 6, cv2.LINE_AA)  # [2][2]
+        # cv2.putText(img, 'X', (1100, 400), font, 4, (200, 180, 100), 6, cv2.LINE_AA)  # [1][2]
+        # cv2.putText(img, 'X', (1100, 150), font, 4, (200, 180, 100), 6, cv2.LINE_AA)  # [0][2]
+
+        # --- experimenting with text - end --- #
+
+        for c in contours:
+            accuracy = 0.01 * cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c, accuracy, True)
+
+            if len(approx) == 4 and cv2.contourArea(approx) > 15000:
+                last_contour = approx
+                cv2.drawContours(img, [approx], -1, (120, 255, 155), 10)
+
+            elif last_contour is not None:
+                cv2.drawContours(img, [last_contour], -1, (150, 255, 155), 3)
+
+        print_cricles(img, last_circles)
+
+        k = cv2.waitKey(2)
+
+
+        if k & 0xFF == ord('q'):
+            if grid is not None:
+                print(f"Grid:\n {grid}")
+
+            # --- part to add -- #
+
+            # new_grid = Game(grid)
+            #
+            # if new_grid is not None:
+            #     print(f"Grid:\n {new_grid}")
+
+            # --- part to add - end --- #
+
+            break
+
+
+        if k & 0xFF == ord('\r'):
+            last_circles = find_circle(processed_img, img, last_circles)
+            grid = circle_position(last_circles, last_contour, img)
+
+            if grid is not None:            # glownie kminie w tej czesci co zrobic zeby pokazywalo x'a dopiero po
+                print(f"Grid:\n {grid}")    # znalezieniu kolejnego kola (no i zeby kola zapisywaly sie w gridzie)
+                new_grid = Game(grid)
+                best_move = findBestMove(grid)
+                best_move_done.append(best_move)
+
+            print(f"new Grid:\n {new_grid}")
+
+        if best_move_done:
+            for i in range(len(best_move_done)):
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                cv2.putText(img, 'X', (x_pos[best_move_done[i][0]][best_move_done[i][1]]), font, 4, (200, 180, 100), 6, cv2.LINE_AA)
+                                        # here we pass the coordinates for displaying x
+                                        # in one of the available squares (depends on the best move)
+
+
+        cv2.imshow('image', img)
+        cv2.imshow('edges', processed_img)
+
+    vid.release()
+    cv2.destroyAllWindows()

005c47954b0307bacfab1868b32668b6b87ce17b
Dominik <swidominik@gmail.com>
2021-12-10 20:31:24 +0100

    extracted function outta main loop

--- dominik_board_detection.py
+++ dominik_board_detection.py
@@ -5,11 +5,13 @@ def preprocess(img):
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     ret, gray = cv2.threshold(gray, 80, 230, cv2.THRESH_BINARY)
 
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    # blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    # blur = cv2.bilateralFilter(gray,9,75,75)
+    blur = gray
 
-    edges = cv2.Canny(gray, 40, 170)
+    edges = cv2.Canny(blur, 40, 170)
     dillated_edges = cv2.dilate(edges.copy(), None, iterations = 5)
-    dillated_edges = cv2.morphologyEx(dillated_edges, cv2.MORPH_CLOSE, np.ones((5,5),np.uint8))
+    dillated_edges = cv2.morphologyEx(dillated_edges, cv2.MORPH_CLOSE, np.ones((8,8),np.uint8))
 
     return dillated_edges 
 
@@ -94,40 +96,38 @@ def circle_position(circles, cnt):
 
                 else:
                     continue
-
-    
-        else:
-            print("No circles!")
-    else:
-        print("No contour!")
     
     return grid_return
 
-
-vid = cv2.VideoCapture(0)
-
-last_contour = None
-last_circles = None
-grid = None
-
-while True:
-    ret, img = vid.read()
-    processed_img = preprocess(img)
-
-    contours, _ = cv2.findContours(processed_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+def find_print_contour(src, dst, prev_contour):
+    contours, _ = cv2.findContours(src, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 
     for c in contours:
         accuracy = 0.01 * cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, accuracy, True)
         
-        if len(approx) == 4 and cv2.contourArea(approx) > 15000:
-            last_contour = approx
-            cv2.drawContours(img, [approx], -1, (120,255,155), 10)
+        if len(approx) == 4 and cv2.contourArea(approx) > 15000:   
+            cv2.drawContours(dst, [approx], -1, (120,255,155), 10)
+            return approx
         
-        elif last_contour is not None:
-            cv2.drawContours(img, [last_contour], -1, (150,255,155), 3)
+    if prev_contour is not None:
+        cv2.drawContours(dst, [prev_contour], -1, (150,255,155), 3)
+        return prev_contour
 
-    last_circles = find_circle(processed_img, last_circles)
+
+vid = cv2.VideoCapture(0)
+
+contour = None
+last_circles = None
+grid = None
+
+while True:
+    ret, img = vid.read()
+    edges = preprocess(img)
+
+    contour = find_print_contour(edges, img, contour)
+
+    last_circles = find_circle(edges, last_circles)
     print_cricles(img, last_circles)
 
     k = cv2.waitKey(50)
@@ -138,16 +138,11 @@ while True:
         break
 
     if k & 0xFF == ord('\r'):
-        grid = circle_position(last_circles, last_contour)
-        if grid is not None:
-            print(grid)
-
-    if k & 0xFF == ord('r'):
-        grid = None
+        grid = circle_position(last_circles, contour)
         print(grid)
-    
+ 
     cv2.imshow('image', img)
-    cv2.imshow('edges', processed_img)
+    cv2.imshow('edges', edges)
         
 vid.release()
 cv2.destroyAllWindows()
